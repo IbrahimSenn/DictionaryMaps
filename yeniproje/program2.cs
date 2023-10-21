b510d187e5830bf509022e5862f35ccd3afda931
Monsterter <ibrahimsenc@gmail.com>
2023-10-21 14:32:02 +0300

    second commit

---
+++ yeniproje/program2.cs
@@ -0,0 +1,11 @@
+namespace Day_8_Dictionaries_and_Maps.yeniproje
+{
+    public class program2
+    {
+        public void Method(string parameter)
+        {
+            Console.WriteLine("merhaba");
+        }
+        
+    }
+}
